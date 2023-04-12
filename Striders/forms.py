f91351b9f986bed268107a00d9c8086ba9d8f5b8
chester <chesterraedevera@gmail.com>
2023-04-11 22:28:00 -0400

    file added

---
+++ Striders/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields import EmailField
+from wtforms.validators import InputRequired, EqualTo, Email, Length, Regexp
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[InputRequired(),
+                                       Length(4, 64),
+                                       Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                              'Usernames must start with a letter and must have only letters, '
+                                              'numbers, dots or underscores')])
+    email = EmailField('Email', validators=[InputRequired()])
+    phone = StringField('Phone number', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(8)])
+    confirmPassword = PasswordField('Confirm Password',
+                                    validators=[InputRequired(),
+                                                EqualTo('password',
+                                                        message='Passwords must match.')])
+    submit = SubmitField('Register')
